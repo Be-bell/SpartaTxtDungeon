d552c932e4312e82ff634a5072d4a7d132291d21
Be-bell <whdghk1346@khu.ac.kr>
2024-05-02 00:23:58 +0900

    Feat : Skill Part(Warrior_1)
    전사 1번째 스킬 추가.
    
    (** 추후 리팩토링 필요해보임.)

---
+++ Sparta2weekProject/Objects/Charactor/SkillBook.cs
@@ -0,0 +1,31 @@
+﻿namespace Sparta2weekProject.Objects.Charactor
+{
+    public class SkillBook
+    {
+        public Skills skill_1 { get; protected set; }
+        public Skills skill_2 { get; protected set; }
+
+        public void SkillsPrint()
+        {
+            Console.WriteLine("[1] " + skill_1.SkillInfo());
+        }
+    }
+
+    public class WarriorSkillBook : SkillBook
+    {
+        public WarriorSkillBook()
+        {
+            skill_1 = new WarriorSkill_1();
+            //skill_2 = new WarriorSkill_2();
+        }
+    }
+
+    public class ArchorSkillBook : SkillBook
+    {
+        public ArchorSkillBook()
+        {
+            //skill_1 = new ArchorSkill_1();
+            //skill_2 = new ArchorSkill_2();
+        }
+    }
+}
