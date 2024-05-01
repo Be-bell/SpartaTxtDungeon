68e2ba6176cb03831402e83a79eba316e6add036
Be-bell <whdghk1346@khu.ac.kr>
2024-05-02 00:20:30 +0900

    Fix, Refactor : Battle Part Fix, Refactor
    전투 파트 버그 수정, 리팩토링
    공격하면 전체 공격 -> 단일로 공격
    몬스터를 모두 처치하면 나오기
    
    (*추후 다시 리팩토링 필요해보임.)

---
+++ Sparta2weekProject/Menu/BattleSystem/Battle.cs
@@ -0,0 +1,234 @@
+﻿using Sparta2weekProject.Objects.Charactor;
+using System.Threading;
+
+namespace Sparta2weekProject.Menu.BattleSystem
+{
+    // 전투 클래스
+    public class Battle : GameManager
+    {
+        int DeadMonster;
+
+        public Battle(Charactor _charactor, Monster[] _monsters)
+        {
+            charactor = _charactor;
+            monsters = _monsters;
+        }
+
+        // 전투 시작 메서드
+        public void InBattle()
+        {
+            DeadMonster = 0;
+            PlayerTurn(); // 플레이어의 턴 시작
+        }
+
+        // 플레이어 턴 메서드
+        private void PlayerTurn()
+        {
+            #region ConsolePrint
+            DisplayMonsters(false);
+            DisplayPlayerInfo();
+            Console.WriteLine($"\n{charactor.Name}의 턴");
+            Console.WriteLine("1. 공격");
+            Console.WriteLine("2. 스킬");
+            Console.Write(">> ");
+            #endregion ConsolePrint
+
+            int choice = GetChoice();
+
+
+            switch (choice)
+            {
+                case 1:
+                    PlayerAttack();
+                    if(!MonsterAllDead())
+                    {
+                        EnemyTurn();
+                    }
+                    break;
+                case 2:
+                    PlayerSkill();
+                    if (!MonsterAllDead())
+                    {
+                        EnemyTurn();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("잘못된 입력입니다.");
+                    PlayerTurn();
+                    break;
+            }
+        }
+
+        private bool MonsterAllDead()
+        {
+            foreach(Monster monster in monsters)
+            {
+                if (monster.IsDead())
+                {
+                    DeadMonster++;
+                }
+            }
+
+            if (DeadMonster == monsters.Length)
+            {
+                // 플레이어 마나 회복
+                charactor.RecoverMana(10);
+                return true;
+            }
+
+            return false;
+        }
+
+        // 플레이어 공격 메서드
+        private void PlayerAttack()
+        {
+            
+
+            DisplayMonsters(true);
+            DisplayPlayerInfo();
+
+            Console.WriteLine("공격할 몬스터를 선택하세요.");
+            Console.WriteLine("0. 돌아가기");
+            int choice = GetChoice();
+
+            if (choice < 0 || choice > monsters.Length)
+            {
+                Console.WriteLine("잘못 지정하였습니다. 다시 선택해주세요.\n");
+                PlayerAttack();
+                return;
+            }
+            else if (choice == 0)
+            {
+                PlayerTurn();
+                return;
+            }
+
+            Monster monster = monsters[choice - 1];
+
+            // 선택한 몬스터가 이미 죽어있으면
+            if (monster.IsDead())
+            {
+                Console.WriteLine("이미 죽은 몬스터입니다.\n");
+                PlayerAttack();
+                return;
+            }
+
+            Console.WriteLine($"[{charactor.Name} 의 공격]");
+            int damage = charactor.Attack;
+
+            Thread.Sleep(1000);
+
+            // 치명타 여부 확인
+            if (IsCritical())
+            {
+                damage = (int)(damage * 1.6); // 데미지 160% 증가
+                Console.WriteLine($"{charactor.Name} 의 공격! {monster.Name}에게 치명타 공격! {damage}의 피해를 입혔습니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{charactor.Name} 의 공격! {monster.Name}에게 {damage}의 피해를 입혔습니다.");
+            }
+
+            // 공격 처리
+            monster.TakeDamage(damage);
+
+            Thread.Sleep(1000);
+        }
+
+        // 적 턴 메서드
+        private void EnemyTurn()
+        {
+            Console.WriteLine("\n[적의 턴]");
+
+            Thread.Sleep(1000);
+            foreach (var monster in monsters)
+            {
+                if (!monster.IsDead())
+                {
+                    // 회피 여부 확인
+                    if (IsDodge())
+                    {
+                        Console.WriteLine($"{charactor.Name}의 공격! {monster.Name}의 공격을 회피했습니다.");
+                    }
+                    else
+                    {
+                        charactor.TakeDamage(monster.ATK);
+                        Console.WriteLine($"{monster.Name}의 공격! {charactor.Name}가 {monster.ATK}의 피해를 입었습니다.");
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+
+
+            Console.Clear();
+            PlayerTurn(); // 플레이어의 턴으로 돌아감
+        }
+
+        // 치명타 여부를 결정하는 메서드
+        private static bool IsCritical()
+        {
+            Random random = new Random();
+
+            //1부터 100까지의 난수
+            int chance = random.Next(1, 101); 
+
+            //15% 확률로 치명타 발생
+            return chance <= 15; 
+        }
+
+        // 회피 여부를 결정하는 메서드
+        private static bool IsDodge()
+        {
+            Random random = new Random();
+
+            //1부터 100까지의 난수
+            int chance = random.Next(1, 101); 
+
+            //10% 확률로 회피 발생
+            return chance <= 10; 
+        }
+
+        // 사용자 입력 처리 메서드
+        private int GetChoice()
+        {
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("숫자를 입력하세요.");
+                return GetChoice();
+            }
+            Console.Clear();
+            return choice;
+        }
+
+        //플레이어 스킬 메서드
+        private void PlayerSkill()
+        {
+            DisplayMonsters(false);
+            DisplayPlayerInfo();
+            Console.WriteLine("[스킬 선택]");
+            SkillBook skills = charactor.SkillBook;
+            skills.SkillsPrint();
+            int choice = GetChoice();
+
+            switch (choice)
+            {
+                case 1:
+                    bool skillUse = charactor.SkillUse(skills.skill_1, charactor, monsters);
+                    if (!skillUse) PlayerSkill();
+                    break;
+                case 2:
+                    Console.WriteLine("스킬 2 사용");
+                    break;
+                case 0:
+                    PlayerTurn(); // 플레이어의 턴으로 돌아감
+                    break;
+                default:
+                    Console.WriteLine("잘못된 입력입니다.");
+                    PlayerSkill();
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Sparta2weekProject/Menu/BattleSystem/Monster.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta2weekProject.Menu.BattleSystem
+{
+    // 몬스터 클래스
+    public class Monster
+    {
+        private string v1;
+        private int v2;
+        private int v3;
+
+        // 필드
+        public string Name { get; } // 몬스터 이름
+        public int Level { get; } // 몬스터 레벨
+        public int HP { get; private set; } // 몬스터 체력
+        public int ATK { get; } // 몬스터 공격력
+
+        // 생성자
+        public Monster(string name, int level, int hp, int atk)
+        {
+            Name = name;
+            Level = level;
+            HP = hp;
+            ATK = atk;
+        }
+
+        /* 
+         public Monster(string v1, int v2, int v3)
+         {
+             this.v1 = v1;
+             this.v2 = v2;
+             this.v3 = v3;
+         }
+        */
+
+        // 몬스터가 피해를 받는 메서드
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+            if (HP <= 0)
+            {
+                HP = 0;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine($"{Name}이(가) 사망했습니다.");
+                Console.ResetColor();
+            }
+        }
+
+        // 몬스터가 죽었는지 확인하는 메서드
+        public bool IsDead()
+        {
+            return HP <= 0;
+        }
+    }
+
+    // 몬스터 정보 정의
+    public static class MonsterInfo
+    {
+        public static Monster Lv2_Minion => new Monster("Lv2 미니언", 2, 15, 5);
+        public static Monster Lv3_VoidBug => new Monster("Lv3 공허충", 3, 10, 9);
+        public static Monster Lv5_CannonMinion => new Monster("Lv5 대포미니언", 5, 25, 8);
+    }
+}
