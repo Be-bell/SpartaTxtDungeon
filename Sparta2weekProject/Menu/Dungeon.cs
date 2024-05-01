b7d8b14ac1cb06968648a19bf3f5d606f0922d8b
ACEDIA2567 <sdman2567@gmail.com>
2024-05-01 14:41:18 +0900

    Feat: 몬스터 랜덤 스폰, 던전 추가 기능
    몬스터 랜덤 스폰
    1. 난이도에 따른 몬스터 정보 저장
    2. 저장한 정보를 랜덤으로 하여 스폰
    
    던전추가
    1. 꼭대기(5층)도달 후 로비로 들어가면 도달했다는 텍스트와 함께 던전에서 나가짐

--- Sparta2weekProject/Menu/Dungeon.cs
+++ Sparta2weekProject/Menu/Dungeon.cs
@@ -1,9 +1,9 @@
 
-﻿using Sparta2weekProject.Objects;
+using Sparta2weekProject.Objects;
 using System.Drawing;
 using System.Reflection.Emit;
 
-﻿using Sparta2weekProject.BattleSystem;
+using Sparta2weekProject.BattleSystem;
 
 
 
@@ -15,7 +15,7 @@ namespace Sparta2weekProject.Menu
         Random random;
         int floor = 1; // 현재 던전 층
 
-        public Dungeon() 
+        public Dungeon()
         {
             menu = 3;
         }
@@ -36,7 +36,7 @@ namespace Sparta2weekProject.Menu
 
             // 선택 시 로직
             choice = base.Choice(menu, true);
-            switch(choice)
+            switch (choice)
             {
                 case 1:
                     DungeonSelect(DungeonLv.쉬움, 10);
@@ -75,53 +75,15 @@ namespace Sparta2weekProject.Menu
                     DungeonSelect(level, def);
                     break;
                 case 2:
-                    // 던전 입장
-                    //DungeonInvite(floor, level);
-                   
                     DungeonExplore(level, def);
                     break;
             }
         }
 
-        // 던전 입장
-        void DungeonInvite(int floor, DungeonLv level)
-        {
-            // 몬스터 개수
-            int monsterCount;
-            // 강한 몬스터
-            int monsterAttack;
-            // int monsterDeffend = random.Next();
-
-            // 난이도에 따른 보상
-            switch (level)
-            {
-                case DungeonLv.쉬움:
-                    monsterCount = random.Next(1, 2);
-                    break;
-                case DungeonLv.일반:
-                    monsterCount = random.Next(1, 4);
-                    monsterAttack = random.Next(0, 3);
-                    break;
-                case DungeonLv.어려움:
-                    monsterCount = random.Next(2, 4);
-                    monsterAttack = random.Next(3, 7);
-                    break;
-            }
-
-            // 몬스터 소환 및 값 공격력 변경
-            // 전투 시작
-
-            // 보상 획득
-
-
-            // 전투 끝으로 층 상승
-            floor++;
-        }
-
         // 던전 탐색
         void DungeonExplore(DungeonLv _lv, int _recommandedDef)
         {
-            int getReward=0;
+            int getReward = 0;
             int getExp = 0;
             int minusHp = 0;
             random = new Random();
@@ -148,17 +110,17 @@ namespace Sparta2weekProject.Menu
 
             minusHp = random.Next(20, 36) + (_recommandedDef - charactor.Defend);
 
-            if ((_recommandedDef > charactor.Defend && random.Next(1,100) > 25) || _recommandedDef > charactor.Health)
+            if ((_recommandedDef > charactor.Defend && random.Next(1, 100) > 25) || _recommandedDef > charactor.Health)
             {
                 //공략 실패
                 Console.WriteLine("던전 공략 실패");
                 Console.WriteLine("던전 공략에 실패했습니다.\n");
                 getReward = 0;
                 minusHp = 50;
-                if(minusHp > charactor.Health)
+                if (minusHp > charactor.Health)
                     minusHp = charactor.Health;
                 getExp -= 10;
-                if(getExp < 0) 
+                if (getExp < 0)
                     getExp = 0;
             }
             else
@@ -167,12 +129,13 @@ namespace Sparta2weekProject.Menu
                 Console.WriteLine("던전 클리어");
                 Console.WriteLine("축하합니다!!");
                 Console.WriteLine($"던전 : {_lv} 을 클리어 했습니다.\n");
+                floor++; // 클리어 후 층 증가
             }
 
             // 보상 수령 및 hp 감소
             int nextExp = charactor.Exp + getExp;
             int level = charactor.Level;
-            if(nextExp >= 200)
+            if (nextExp >= 200)
             {
                 Console.WriteLine("레벨업 하였습니다!\n");
                 level++;
@@ -181,8 +144,8 @@ namespace Sparta2weekProject.Menu
                 nextExp -= 200;
             }
 
-            float rewardPercent = (random.Next(charactor.Attack, (charactor.Attack*2) + 1) + 100) / 100.0f;
-            int totalReward = (int) (getReward * rewardPercent);
+            float rewardPercent = (random.Next(charactor.Attack, (charactor.Attack * 2) + 1) + 100) / 100.0f;
+            int totalReward = (int)(getReward * rewardPercent);
             Console.WriteLine("[탐험 결과]");
             Console.WriteLine($"체력 {charactor.Health} -> {charactor.Health - minusHp}");
             Console.WriteLine($"Gold {charactor.Gold} -> {charactor.Gold + totalReward}");
@@ -192,15 +155,27 @@ namespace Sparta2weekProject.Menu
             charactor.Gold = charactor.Gold + totalReward;
 
             // 캐릭터 사망
-            if(charactor.Health == 0)
+            if (charactor.Health == 0)
             {
                 Console.WriteLine("\n캐릭터가 사망했습니다.");
                 Console.WriteLine("게임 오버");
             }
 
-            Console.WriteLine("\n0. 나가기\n");
-
-            choice = base.Choice(0, true);
+            Console.WriteLine("\n0. 던전 나가기");
+            Console.WriteLine("1. 던전 로비\n");
+            choice = base.Choice(1, true);
+            if (choice == 1)
+            {
+                if (floor == 6)
+                {
+                    Console.WriteLine("꼭대기에 도착하여 던전에서 나갑니다.\n");
+                }
+                else
+                {
+                    // 던전 로비로 이동
+                    DungeonSelect(_lv, _recommandedDef);
+                }   
+            }
         }
 
         // 전투 시작
@@ -224,36 +199,58 @@ namespace Sparta2weekProject.Menu
         // 던전 레벨에 따라 몬스터 생성
         Monster[] CreateMonsters(DungeonLv dungeonLevel)
         {
+            int monsterCount; // 몬스터 개체 수
+            int monsterAttack = 0; // 층에 따른 몬스터 추가 공격력
+            int monsterHp = 0; // 층에 따른 몬스터 추가 체력
+            Monster[] MonstersLevel; // 난이도에 따른 몬스터 정보
             switch (dungeonLevel)
             {
                 case DungeonLv.쉬움:
-                    return new Monster[]
+                    monsterCount = random.Next(1, 3);
+                    MonstersLevel = new Monster[]
                     {
                           new Monster("미니언", 15, 20, 3),
                           new Monster("대포미니언", 25, 20,4),
                           new Monster("공허충", 10, 30, 6)
                     };
+                    break;
                 case DungeonLv.일반:
-                    return new Monster[]
+                    monsterCount = random.Next(1, 4);
+                    monsterAttack = random.Next(0, 3);
+                    monsterHp = random.Next(0, 5);
+                    MonstersLevel = new Monster[]
                     {
-                        new Monster("미니언", 15, 25,5),
-                        new Monster("대포미니언", 25, 30,6),
-                        new Monster("공허충", 10, 50,8)
+                        new Monster("미니언", 15, 25 + monsterHp, 5 + monsterAttack),
+                        new Monster("대포미니언", 25, 30 + monsterHp,6 + monsterAttack),
+                        new Monster("공허충", 10, 50 + monsterHp, 8 + monsterAttack)
                     };
+                    break;
                 case DungeonLv.어려움:
-                    return new Monster[]
+                    monsterCount = random.Next(2, 4);
+                    monsterAttack = random.Next(3, 7);
+                    monsterHp = random.Next(5, 15);
+                    MonstersLevel = new Monster[]
                     {
-                        new Monster("미니언", 15, 40, 8),
-                        new Monster("대포미니언", 25, 60, 9),
-                        new Monster("공허충", 10,60, 10)
+                        new Monster("미니언", 15, 40 + monsterHp, 8 + monsterAttack),
+                        new Monster("대포미니언", 25, 60 + monsterHp, 9 + monsterAttack),
+                        new Monster("공허충", 10, 60 + monsterHp, 10 + monsterAttack)
                     };
+                    break;
                 default:
                     throw new ArgumentException("올바르지 않은 던전 레벨입니다.");
             }
+
+            // 해당 몬스터 정보를 토대로 monsterCount만큼 랜덤으로 스폰
+            Monster[] randomMonster = new Monster[monsterCount];
+            for(int i = 0; i < monsterCount; i++)
+            {
+                randomMonster[i] = MonstersLevel[random.Next(0, 3)];
+            }
+            return randomMonster;
         }
     }
     enum DungeonLv
     {
-        쉬움=1, 일반, 어려움
+        쉬움 = 1, 일반, 어려움
     }
 }
